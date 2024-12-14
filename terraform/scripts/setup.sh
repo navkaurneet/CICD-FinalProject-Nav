bf150270fca5e6f65ba0988574bad0285b18452f
navkaurneet <navkaurneet1@gmail.com>
2024-12-14 17:17:11 -0500

    Updating app files

--- terraform/scripts/setup.sh
+++ terraform/scripts/setup.sh
@@ -11,34 +11,12 @@ echo "Creating /app directory..."
 sudo mkdir -p /app
 sudo chown -R $USER:$USER /app
 
+aws s3 cp s3://navk-flask-frontend-bucket/index.html /app/index.html
+
 # Clone the Flask app repository from GitHub (or download it)
 echo "Cloning the repository..."
 git clone https://github.com/navkaurneet/CICD-FinalProject-Nav.git /app/backend
 
-# Check if the clone was successful
-if [ ! -d "/app/backend" ]; then
-    echo "Failed to clone the repository."
-    exit 1
-fi
 
-# Copy the index.html file from the cloned repository to the /app directory
-echo "Copying index.html to /app directory..."
-cp /app/backend/app/static/index.html /app/index.html
-
-# Check if the file copy was successful
-if [ ! -f "/app/index.html" ]; then
-    echo "Failed to copy index.html."
-    exit 1
-fi
-
-# Start the Flask app in the background and log the output
-echo "Starting the Flask app..."
+# Start the Flask app
 nohup python3 /app/backend/app.py > /app/backend/logs.txt 2>&1 &
-
-# Confirm the Flask app has started
-if pgrep -f "python3 /app/backend/app.py" > /dev/null; then
-    echo "Flask app started successfully."
-else
-    echo "Failed to start the Flask app."
-    exit 1
-fi
