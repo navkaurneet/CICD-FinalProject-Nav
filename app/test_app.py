3b3ee7ba2b1e50001747e20c456fe95e7916330b
Navneet Kaur <navkaurneet1@gmail.com>
2024-12-13 22:52:04 -0500

    Create test_app.py a test file for the test stage.

---
+++ app/test_app.py
@@ -0,0 +1,17 @@
+# test_app.py
+import pytest
+
+def test_example_1():
+    assert 1 + 1 == 2
+
+def test_example_2():
+    assert 'hello'.upper() == 'HELLO'
+
+def test_example_3():
+    assert 2 * 2 == 4
+
+def test_example_4():
+    assert 'flask' in 'hello flask'
+
+def test_example_5():
+    assert 5 - 3 == 2
