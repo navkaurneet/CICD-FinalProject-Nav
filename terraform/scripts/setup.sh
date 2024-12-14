36fd159157d97705f8f62f1cc28f24d93644845d
Navneet Kaur <navkaurneet1@gmail.com>
2024-12-14 16:21:27 -0500

    Update setup.sh

--- terraform/scripts/setup.sh
+++ terraform/scripts/setup.sh
@@ -4,13 +4,11 @@ sudo apt update -y
 sudo apt install -y python3 python3-pip
 pip3 install flask
 
-# Download the app
-mkdir /app
-aws s3 cp s3://navk-flask-frontend-bucket/index.html /app/index.html
-
-# Clone the Flask app repository (or download it)
-# Assuming the Flask app code is in a GitHub repo
+# Clone the Flask app repository from GitHub (or download it)
 git clone https://github.com/navkaurneet/CICD-FinalProject-Nav.git /app/backend
 
-# Start the Flask app
+# Copy the index.html from the cloned repository to the desired location
+cp /app/backend/app/static/index.html /app/index.html
+
+# Start the Flask app in the background and log the output
 nohup python3 /app/backend/app.py > /app/backend/logs.txt 2>&1 &
